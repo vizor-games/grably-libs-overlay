d3abb12ac65c97b45b84d9b49c18f9e77a397a33
Viktor Kuzmin <kvaster@gmail.com>
2018-11-16 09:29:59 +0300

    mvel2 java11 compatibility

--- org/mvel/mvel2/mvel2-2.3.2.rb
+++ org/mvel/mvel2/mvel2-2.3.2.rb
@@ -4,8 +4,8 @@ extend JavacLib
 
 @github = { project: 'mvel/mvel', rev: "#{full_name}.Final" }
 @srcs = 'src/main/java'
-@res = 'src/main/resources'
 
 def patch
   patch_w('java11-compat.patch')
 end
+
